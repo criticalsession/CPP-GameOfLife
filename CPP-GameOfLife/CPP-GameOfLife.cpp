57baa9aa44175ff11ffed4f510e11f36921823ad
Amante Reale <criticalsession@protonmail.ch>
2023-06-01 08:45:48 +0200

    init implementation

--- CPP-GameOfLife/CPP-GameOfLife.cpp
+++ CPP-GameOfLife/CPP-GameOfLife.cpp
@@ -1,20 +1,131 @@
-// CPP-GameOfLife.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SDL.h>
+#include <vector>
 
-#include <iostream>
+using std::vector;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Width and height of container
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+// Width and height of cells
+const int CELL_WIDTH = 10;
+const int CELL_HEIGHT = 10;
+
+const int GRID_WIDTH = WIDTH / CELL_WIDTH;
+const int GRID_HEIGHT = HEIGHT / CELL_HEIGHT;
+
+// Vector grid
+vector<vector<bool>> grid(GRID_WIDTH, vector<bool>(GRID_HEIGHT, false));
+
+int getLiveNeighbours(int x, int y) {
+	int liveNeighbours = 0;
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			if (i == 0 && j == 0) continue;
+
+			int nx = x + i;
+			int ny = y + i;
+			if (nx >= 0 && ny >= 0 && nx < GRID_WIDTH && ny < GRID_HEIGHT && grid[nx][ny]) {
+				liveNeighbours++;
+			}
+		}
+	}
+
+	return liveNeighbours;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void updateGrid() {
+	vector<vector<bool>> newGrid = grid;
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = 0; y < GRID_HEIGHT; y++) {
+			int liveNeighbours = getLiveNeighbours(x, y);
+			if (grid[x][y]) { // already alive
+				if (liveNeighbours < 2 || liveNeighbours > 3) {
+					newGrid[x][y] = false; // cell died
+				}
+			}
+			else {
+				if (liveNeighbours == 3) {
+					newGrid[x][y] = true; // cell becomes alive
+				}
+			}
+		}
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	grid = newGrid;
+}
+
+void renderGrid(SDL_Renderer *renderer) {
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = 0; y < GRID_HEIGHT; y++) {
+			SDL_Rect cell;
+			cell.x = x * CELL_WIDTH;
+			cell.y = y * CELL_HEIGHT;
+			cell.w = CELL_WIDTH;
+			cell.h = CELL_HEIGHT;
+
+			if (grid[x][y]) {
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // alive
+			}
+			else {
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // dead
+			}
+
+			SDL_RenderFillRect(renderer, &cell);
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	// Create application window
+	window = SDL_CreateWindow("Game of Life",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WIDTH,
+		HEIGHT,
+		0);
+
+	// Create SDL Renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	// Game loop
+	bool running = true;
+	SDL_Event event;
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
+			
+			if (event.type == SDL_MOUSEBUTTONDOWN) {
+				int x, y;
+				SDL_GetMouseState(&x, &y);
+
+				int gridX = x / CELL_WIDTH;
+				int gridY = y / CELL_HEIGHT;
+
+				grid[gridX][gridY] = !grid[gridX][gridY];
+			}
+		}
+
+		updateGrid();
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		renderGrid(renderer);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(100);
+	}
+
+	// Clean up
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
